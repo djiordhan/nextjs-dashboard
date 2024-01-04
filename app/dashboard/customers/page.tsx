3c503262b8610eac80aee3f3b45f9b275aa4e644
jordan <jordandiamante@icloud.com>
2024-01-04 12:13:32 +0800

    Chapter 15 - authentication

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export default async function Page({
+    searchParams,
+  }: {
+    searchParams?: {
+      query?: string;
+      page?: string;
+    };
+  }) {
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
+
+    return <div className="w-full">
+    <Table customers={customers}/>
+  </div>;
 }
